ff0b6c4f95d1cef5e135ef21892f59eab27816d5
vgerton <191219k1>
2014-02-27 14:29:44 +0300

    Initial commit

---
+++ src/main/java/com/viewer/controller/SignalsViewController.java
@@ -0,0 +1,40 @@
+package com.viewer.controller;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by mikalai on 2/27/14.
+ */
+@RequestMapping("/signalsviewer")
+@Controller
+public class SignalsViewController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String startPage(Model uiModel) {
+        return "test";
+    }
+
+    @RequestMapping(value = "/file", method = RequestMethod.POST)
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        if (!file.isEmpty()) {
+            List rows = IOUtils.readLines(file.getInputStream(), "UTF-8");
+            for (Object row: rows) {
+                System.out.println(row);
+            }
+
+        }
+
+        return "test2";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/test2.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mikalai
+  Date: 2/27/14
+  Time: 1:20 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>test2</title>
+</head>
+<body>
+
+</body>
+</html>
